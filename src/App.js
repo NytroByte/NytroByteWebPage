ae5b416f2f2a1722474b7776e954774279f75edf
KanKan-coding <markusskancans05@gmail.com>
2022-12-22 21:01:08 +0200

    edited landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./header/Header";
+import LandingPage from "./landing_page/landing_page"
 function App() {
   return (
     <div className="App">
       <Header/>
+      <LandingPage/>
     </div>
   );
 }
--- src/landing_page/landing_page.jsx
+++ src/landing_page/landing_page.jsx
@@ -1,7 +1,12 @@
 function LandingPage() {
     return (
         <div className="landingpage_wrapper">
-            
+            <div className="header_text">
+                <h2 className="h2_text">Lorem ipsum sussy</h2>
+                <h3 className="h3_text">you not sussy?</h3>
+            </div>
         </div>
     )
 }
+
+export default LandingPage;
