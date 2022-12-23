aa2cb69a300e0482c8a96dde355a90d5657d4a7f
KanKan-coding <markusskancans05@gmail.com>
2022-12-24 00:15:54 +0200

    landing page continuation

--- src/landing_page/LandingPage.jsx
+++ src/landing_page/LandingPage.jsx
@@ -3,7 +3,7 @@ function LandingPage() {
         <div className="landingpage_wrapper">
             <div className="headertext">
                 <h2>Lorem ipsum sussy</h2>
-                <h3>You not sussy?</h3>
+                <h3>NytroByte?</h3>
                 <button className="about_button" type="button">here</button>
             </div>
             <div className="demotext">
