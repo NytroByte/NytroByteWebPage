8ee5345fa54a9f5c9112c489f6d61767ade85147
KanKan-coding <markusskancans05@gmail.com>
2022-12-26 19:37:04 +0200

    edited contacts and footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./header/Header";
 import LandingPage from "./landing_page/LandingPage"
 import Projects from "./projects/Projects";
 import Contacts from "./contacts/Contacts";
+import Footer from "./footer/Footer";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
       <About/>
       <Projects/>
       <Contacts/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/footer/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div className="footer_wrapper">
+            <div className="footer">
+                <h1>Copyright © 2022 Nytro<mark>Byte</mark> </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
